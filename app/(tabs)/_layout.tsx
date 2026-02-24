ee9d3ed3c1390c47d280597c0f97e62a1655a0e5
ansyar <m.ansyarafi@gmail.com>
2026-02-24 16:09:22 +1100

    style(ui): Rename Tab Two to Settings and update icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -51,8 +51,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
         }}
       />
     </Tabs>
