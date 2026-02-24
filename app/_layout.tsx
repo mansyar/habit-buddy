4d72420f75e7c135dd0d33117931b6d4fe35a275
ansyar <m.ansyarafi@gmail.com>
2026-02-24 16:17:02 +1100

    fix(router): Add (auth) layout and correct root stack definition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -113,7 +113,7 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
